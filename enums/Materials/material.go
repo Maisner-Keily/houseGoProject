04482c94bc880e4ce5ab0d486bf28f8fa42b5073
Maisner-Keily <jamsonkeil@ya.ru>
2024-01-01 20:44:38 +0300

    Добавлен материал пластик

--- enums/Materials/material.go
+++ enums/Materials/material.go
@@ -3,6 +3,7 @@ package Materials
 type Material string
 
 const (
-	Wood  Material = "wood"
-	Metal Material = "metal"
+	Wood    Material = "wood"
+	Metal   Material = "metal"
+	Plastic Material = "plastic"
 )

5b6812737433ea651946a7f3393140374ec5c785
Maisner-Keily <jamsonkeil@ya.ru>
2024-01-06 20:49:49 +0300

    Добавлены enums FamilyRole и Gender

---
+++ enums/FamilyRoles/FamilyRole.go
@@ -0,0 +1,18 @@
+package FamilyRoles
+
+type FamilyRole string
+
+const (
+	Mother       FamilyRole = "mother"
+	Father       FamilyRole = "father"
+	StepMother   FamilyRole = "stepMother"
+	StepFather   FamilyRole = "stepFather"
+	StepSon      FamilyRole = "stepSon"
+	StepDaughter FamilyRole = "stepDaughter"
+	Uncle        FamilyRole = "uncle"
+	Aunt         FamilyRole = "aunt"
+	Son          FamilyRole = "son"
+	Daughter     FamilyRole = "daughter"
+	GrandMother  FamilyRole = "grandMother"
+	GrandFather  FamilyRole = "grandFather"
+)

ab4fed184d2a12c55680bdc88e99a8064dbc5c71
Maisner-Keily <jamsonkeil@ya.ru>
2024-01-06 20:53:37 +0300

    small fix

--- structs/rooms.go
+++ structs/rooms.go
@@ -17,23 +17,13 @@ type Room struct {
 }
 
 func (room Room) getSquare() float32 {
-	return room.width * room.length
+	return room.Width * room.Length
 }
 
 func (room Room) getVolume() float32 {
-	return room.width * room.length * room.height
+	return room.Width * room.Length * room.Height
 }
 
 func (room Room) getPerimeter() float32 {
-	return 2 * (room.width + room.length)
-}
-
-func createRooms() []Room {
-	var rooms []Room
-
-	rooms[0] = Room{width: 5.4, length: 3.4, height: 2.5}
-	rooms[1] = Room{width: 3.2, length: 4.1, height: 3}
-	rooms[2] = Room{width: 4.7, length: 3.2, height: 2.8}
-
-	return rooms
+	return 2 * (room.Width + room.Length)
 }

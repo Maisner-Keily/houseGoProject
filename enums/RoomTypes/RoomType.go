0684a67c007d812673bb1f776b111cd1fa2f5ca4
Maisner-Keily <jamsonkeil@ya.ru>
2024-01-07 20:50:18 +0300

    added RoomTypes enum

---
+++ enums/RoomTypes/RoomType.go
@@ -0,0 +1,11 @@
+package RoomTypes
+
+type RoomType string
+
+const (
+	BedRoom    RoomType = "bedroom"
+	BathRoom   RoomType = "bathroom"
+	Hallway    RoomType = "hallway"
+	Kitchen    RoomType = "kitchen"
+	LivingRoom RoomType = "livingroom"
+)

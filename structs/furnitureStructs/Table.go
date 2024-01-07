95595a8a4717cf4d7d2990878ea2c002fd1325fa
Maisner-Keily <jamsonkeil@ya.ru>
2024-01-07 20:53:19 +0300

    Убрал ключ Sizes

--- structs/furnitureStructs/Table.go
+++ structs/furnitureStructs/Table.go
@@ -1,13 +1,11 @@
 package furnitureStructs
 
 import (
-	"go/types"
 	"houseGoProject/structs"
 )
 
 type Table struct {
 	ID        int
 	Furniture structs.Furniture
-	Sizes     types.Sizes
 	Name      string
 }

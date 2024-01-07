60c953b818f77a04414d2bf19b1bb1df0456aa9a
Maisner-Keily <jamsonkeil@ya.ru>
2024-01-08 00:49:25 +0300

    Свои размеры у каждого типа, в старом решении проблема с циклическими импортами

---
+++ structs/furnitureStructs/furnitureSizes.go
@@ -0,0 +1,7 @@
+package furnitureStructs
+
+type FurnitureSizes struct {
+	Width  float32
+	Length float32
+	Height float32
+}
---
+++ structs/techniqueStructs/techiqueSizes.go
@@ -0,0 +1,7 @@
+package techniqueStructs
+
+type TechniqueSizes struct {
+	Width  float32
+	Length float32
+	Height float32
+}
